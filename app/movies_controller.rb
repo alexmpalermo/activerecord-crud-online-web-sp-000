41ca5d8277a103230b1583ee1b6d6d82504a2e33
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-01 20:25:49 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,8 +74,8 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  movie = Movie.update(title: "Wat, huh?")
+  movie = Movie.create(title: "Wat?")
+  movie.update(title: "Wat, huh?")
   movie
 end
 
